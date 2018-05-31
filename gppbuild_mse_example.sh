ca2ebb7af531743188dcc1b37b107d40176166f8
duneroadrunner <duneroadrunner@users.noreply.github.com>
2018-05-31 10:36:46 -0700

    Update gppbuild_mse_example.sh

--- gppbuild_mse_example.sh
+++ gppbuild_mse_example.sh
@@ -1,1 +1,1 @@
-g++ -std=c++14 -pthread msetl_example.cpp msetl_example2.cpp mserelaxedregistered.cpp -o msetl_example.gpp_run
+g++ -std=c++14 -pthread msetl_example.cpp msetl_example2.cpp -o msetl_example.gpp_run
