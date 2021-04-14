6516220f284d533ea0cd98973bffa3b8e2110918
x <x@y.com>
2021-04-14 14:01:19 -0700

    added operator*() to lh::TNativeArrayReplacement<>

--- mselegacyhelpers.h
+++ mselegacyhelpers.h
@@ -514,6 +514,9 @@ namespace mse {
 				&& (!std::is_same<typename mse::mstd::array<_Ty2, _Size>::const_iterator, typename mse::mstd::array<_Ty2, _Size>::iterator>::value)> MSE_IMPL_EIS >
 			typename base_class::difference_type operator-(const typename base_class::const_iterator& _Right_cref) const { return base_class::cbegin() - _Right_cref; }
 
+			typename base_class::const_reference operator*() const { return base_class::operator*(); }
+			typename base_class::const_pointer operator->() const { return base_class::operator->(); }
+
 #ifdef MSE_LEGACYHELPERS_DISABLED
 			TNativeArrayReplacement(_XSTD initializer_list<_Ty> _Ilist) : base_class(mse::nii_array<_Ty, _Size>(_Ilist)) {}
 #endif // MSE_LEGACYHELPERS_DISABLED
@@ -1038,7 +1041,6 @@ namespace mse {
 					MSE_IMPL_LH_EXPLICITLY_CASTABLE_ANY_TYPE_CHECK1(float);
 					MSE_IMPL_LH_EXPLICITLY_CASTABLE_ANY_TYPE_CHECK1(double);
 					MSE_IMPL_LH_EXPLICITLY_CASTABLE_ANY_TYPE_CHECK1(long double);
-					MSE_IMPL_LH_EXPLICITLY_CASTABLE_ANY_TYPE_CHECK1(std::string);
 
 					return mse::any_cast<T>(*this);
 				}
